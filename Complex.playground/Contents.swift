2b813c46e368acd1462b9b0af53bbe5ad8e21091
Dan Kogai <dankogai+github@gmail.com>
2016-01-09 21:59:15 +0900

    update playground

--- Complex.playground/Contents.swift
+++ Complex.playground/Contents.swift
@@ -15,17 +15,18 @@ z1 - z0 == z0
 z1 / z0 == z0
 sqrt(z1) == z0
 let epi = exp(Double.PI.i)
-epi == -1
-epi =~ -1
-log(-1+0.i).i
-tan(z0) == sin(z0)/cos(z0)
-(sin(1.i)**2 + cos(1.i)**2) =~ 1
-asin(sin(0.5.i))
-acos(cos(0.5.i))
-atan(tan(0.5.i))
-sinh(0.5) == -sin(0.5.i).i
-cosh(0.5) == cos(0.5.i)
-tanh(0.5+0.i) =~ -tan(0.5.i).i
-asinh(sinh(0.5.i))
-acosh(cosh(0.5.i))
-atanh(tanh(0.5.i))
+epi == -1   // sorry, floating point is inexact
+epi =~ -1   // but you can use this!
+log(-1+0.i) // πi
+let z2 = sqrt(-1.i)
+tan(z2) == sin(z2)/cos(z2)
+(sin(z2)**2 + cos(z2)**2) =~ 1
+asin(sin(z2)) =~ z2
+acos(cos(z2)) =~ z2
+atan(tan(z2)) =~ z2
+sinh(z2) == -sin(z2).i
+cosh(z2) == cos(z2.i)
+tanh(z2) =~ -tan(z2.i).i
+asinh(sinh(z2)) =~ z2
+acosh(cosh(z2)) =~ z2
+atanh(tanh(z2)) =~ z2
