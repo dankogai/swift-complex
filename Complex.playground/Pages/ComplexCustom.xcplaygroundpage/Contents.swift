22d3431f33eecfc274755c0f36d156d7eb3c16ad
Dan Kogai <dankogai+github@gmail.com>
2016-01-22 21:25:50 +0900

    update Complex.playground

--- Complex.playground/Pages/ComplexCustom.xcplaygroundpage/Contents.swift
+++ Complex.playground/Pages/ComplexCustom.xcplaygroundpage/Contents.swift
@@ -3,45 +3,98 @@
 ## Advanced Topic: Using Complex<CustomNumberType>
 
 So long as `CustomNumberType` conforms to `ArithmeticType`, Complex<CustomNumberType> works (with issues.  See ([ComplexRat](@next) for such cases that are supposed to work but crashes Swift).
+*/
+/*:
+### Example: TinyInt
 
-### Complex<Bool>
-
-To demonstrate that, let us make `Complex<Bool>`.  To do so, we first make `Bool` conform to `ArithmeticType`
+To demonstrate that, let us make `TinyInt`, consisting only of `-1`, `0`, and '1'.
 
 */
-public prefix func +(b:Bool)->Bool {
-    return b
+public enum TinyInt:Int8 {
+    case Z =  0
+    case P =  1
+    case N = -1
 }
-public prefix func -(b:Bool)->Bool {
-    return !b
+//: cross-platform import
+#if os(Linux)
+    import Glibc
+#else
+    import Foundation
+#endif
+//: required initializers
+public extension TinyInt {
+    public init(_ t:TinyInt) { self = t }
+    public init(_ i:Int8) {
+        self = i < 0 ? .N : i > 0 ? .P : .Z
+    }
+    public init(_ d:Int)    { self.init(Int8(d)) }
+    public init(_ d:Double) { self.init(Int8(d)) }
+    public init(_ f:Float)  { self.init(Int8(f)) }
+    #if !os(Linux)
+    public init(_ c:CGFloat)    { self.init(Int(c)) }
+    #endif
 }
-public func +(lhs:Bool, rhs:Bool)->Bool {
-    return lhs || rhs
-}
-public func *(lhs:Bool, rhs:Bool)->Bool {
-    return lhs && rhs
-}
-public func -(lhs:Bool, rhs:Bool)->Bool {
-    return lhs + (-rhs)
-}
-public func /(lhs:Bool, rhs:Bool)->Bool {
-    return lhs * rhs
-}
-public func <(lhs:Bool, rhs:Bool)->Bool {
-    return lhs == false && rhs == true
-}
-extension Bool : IntegerLiteralConvertible {
+//: protocol conformance
+extension TinyInt : Comparable, IntegerLiteralConvertible, CustomStringConvertible {
     public typealias IntegerLiteralType = Int
     public init(integerLiteral i:Int) {
         self.init(Int(i))
     }
-    public var isSignMinus:Bool { return false }
+    public var description:String {
+        return self.rawValue.description
+    }
 }
-extension Bool: ArithmeticType {}
-//: Now let's see if it behaves as a complex number
-let one = Complex(true, false)
-one.i == one.conj
-one.i.i == -one
-one.i.i.i ==  (-one).conj
-one.i.i.i.i == one
+public func < (lhs:TinyInt, rhs:TinyInt)->Bool {
+    return lhs.rawValue < rhs.rawValue
+}
+//: and arithmetic operators
+public prefix func + (t:TinyInt)->TinyInt {
+    return t
+}
+public prefix func - (t:TinyInt)->TinyInt {
+    switch t {
+    case .N: return .P
+    case .P: return .N
+    default: return .Z
+    }
+}
+public func + (lhs:TinyInt, rhs:TinyInt)->TinyInt {
+    return TinyInt(lhs.rawValue + rhs.rawValue)
+}
+public func - (lhs:TinyInt, rhs:TinyInt)->TinyInt {
+    return TinyInt(lhs.rawValue - rhs.rawValue)
+}
+public func * (lhs:TinyInt, rhs:TinyInt)->TinyInt {
+    return TinyInt(lhs.rawValue * rhs.rawValue)
+}
+public func / (lhs:TinyInt, rhs:TinyInt)->TinyInt {
+    return TinyInt(lhs.rawValue / rhs.rawValue)
+}
+//: let's first see TynyInt works as expected.
+TinyInt.Z + TinyInt.Z
+TinyInt.Z + TinyInt.P
+TinyInt.Z + TinyInt.N
+TinyInt.P + TinyInt.Z
+TinyInt.P + TinyInt.P
+TinyInt.P + TinyInt.N
+TinyInt.N + TinyInt.Z
+TinyInt.N + TinyInt.P
+TinyInt.N + TinyInt.N
+TinyInt.Z * TinyInt.Z
+TinyInt.Z * TinyInt.P
+TinyInt.Z * TinyInt.N
+TinyInt.P * TinyInt.Z
+TinyInt.P * TinyInt.P
+TinyInt.P * TinyInt.N
+TinyInt.N * TinyInt.Z
+TinyInt.N * TinyInt.P
+TinyInt.N * TinyInt.N
+//: now let's tell Swift `TinyInt` conforms to `ArithmeticType`
+extension TinyInt:ArithmeticType {}
+//: and enjoy!
+let z = Complex(TinyInt.P, TinyInt.P)
+z.i
+z.i.i == -z
+z.i.i.i == z.conj
+z.i.i.i.i == z
 //: [Next](@next)
